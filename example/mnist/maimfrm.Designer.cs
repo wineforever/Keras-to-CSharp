7eec6f45f2d19d7a0fed13d3806f8f51315be282
wineforever <zyfdiaoabaole@163.com>
2020-03-10 12:50:12 +0800

    add example

---
+++ example/mnist/maimfrm.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace mnist
+{
+    partial class mainfrm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.inputbox = new System.Windows.Forms.PictureBox();
+            this.res = new System.Windows.Forms.Label();
+            this.predictBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.inputbox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // inputbox
+            // 
+            this.inputbox.Location = new System.Drawing.Point(12, 12);
+            this.inputbox.Name = "inputbox";
+            this.inputbox.Size = new System.Drawing.Size(280, 280);
+            this.inputbox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.inputbox.TabIndex = 0;
+            this.inputbox.TabStop = false;
+            this.inputbox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.inputbox_MouseClick);
+            this.inputbox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.inputbox_MouseDown);
+            this.inputbox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.inputbox_MouseUp);
+            // 
+            // res
+            // 
+            this.res.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.res.Font = new System.Drawing.Font("华文宋体", 99.74999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.res.Location = new System.Drawing.Point(298, 13);
+            this.res.Name = "res";
+            this.res.Size = new System.Drawing.Size(280, 280);
+            this.res.TabIndex = 1;
+            this.res.Text = "0";
+            this.res.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // predictBtn
+            // 
+            this.predictBtn.Location = new System.Drawing.Point(584, 13);
+            this.predictBtn.Name = "predictBtn";
+            this.predictBtn.Size = new System.Drawing.Size(91, 29);
+            this.predictBtn.TabIndex = 2;
+            this.predictBtn.Text = "&Predict";
+            this.predictBtn.UseVisualStyleBackColor = true;
+            this.predictBtn.Click += new System.EventHandler(this.predictBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Location = new System.Drawing.Point(584, 48);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(91, 29);
+            this.exitBtn.TabIndex = 3;
+            this.exitBtn.Text = "&Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // mainfrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 302);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.predictBtn);
+            this.Controls.Add(this.res);
+            this.Controls.Add(this.inputbox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "mainfrm";
+            this.Text = "mnist ocr";
+            this.Load += new System.EventHandler(this.mainfrm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.inputbox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox inputbox;
+        private System.Windows.Forms.Label res;
+        private System.Windows.Forms.Button predictBtn;
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ example/mnist/maimfrm.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Linq.Expressions;
+
+namespace mnist
+{
+    public partial class mainfrm : Form
+    {
+        public mainfrm()
+        {
+            InitializeComponent();
+        }
+        public Wineforever.Neuralnet.System sys;
+        private void mainfrm_Load(object sender, EventArgs e)
+        {
+            //初始化手写操作
+            handwriting_ini();
+            //构建BP神经网络
+            sys = new Wineforever.Neuralnet.System("sys");
+            sys.Create(32);
+            sys.Create(10, "", "softmax");
+            //加载权重
+            sys.Load(System.AppDomain.CurrentDomain.BaseDirectory + "Model\\");
+        }
+        private void predictBtn_Click(object sender, EventArgs e)
+        {
+            //耗时计算
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            //预处理
+            bitmap = new Bitmap(bitmap, 28, 28);
+            BitmapData data = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            var input = new List<double>();
+            unsafe//指针取色
+            {
+                for (int i = 0; i < bitmap.Width; i++)
+                    for (int j = 0; j < bitmap.Height; j++)
+                    {
+                        byte* ptr = (byte*)data.Scan0 + j * 3 + i * data.Stride;
+                        input.Add(*ptr / 255);
+                    }
+            }
+            bitmap.UnlockBits(data);
+            //载入数据
+            sys.Input(input);
+            //预测
+            List<double> output = sys.GetOutput();
+            var sorted = output.Select((x, i) => new KeyValuePair<double, int>(x, i)).OrderByDescending(x => x.Key).ToList();
+            var index = sorted.Select(i => i.Value).ToList()[0];
+            res.Text = index.ToString();
+            //打印
+            stopwatch.Stop();
+            Console.WriteLine("总用时(毫秒)：" + stopwatch.ElapsedMilliseconds);
+        }
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        #region 写字过程
+        private Bitmap bitmap;
+        private Graphics g;
+        //初始化
+        private void handwriting_ini()
+        {
+            bitmap = new Bitmap(inputbox.Width, inputbox.Height);
+            g = Graphics.FromImage(bitmap);
+            g.FillRectangle(Brushes.Black, 0, 0, inputbox.Width, inputbox.Height);
+            inputbox.Image = bitmap;
+        }
+        //右键清除绘图痕迹
+        private void inputbox_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                handwriting_ini();
+            }
+        }
+        //开始写字
+        private void inputbox_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+                timer.Enabled = true;
+        }
+        //结束写字
+        private void inputbox_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+                timer.Enabled = false;
+        }
+        //写字过程
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            g.FillEllipse(Brushes.White, inputbox.PointToClient(Control.MousePosition).X, inputbox.PointToClient(Control.MousePosition).Y, 20, 20);
+            inputbox.Image = bitmap;
+        }
+        #endregion
+    }
+}
